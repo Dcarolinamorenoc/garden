546e281296e16e3145ba1b0dd9f0ef57afab91b2
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-04-19 12:31:01 -0500

    feat: :construction: Usando comandos de Javascript

--- js/bill.js
+++ js/bill.js
@@ -1,2 +1,19 @@
-import {example} from "./component/shopBillCamper.js"
-console.log(example())
+import {getAllProductBill} from "./component/shopBillCamper.js"
+
+let myBill = document.querySelector('#myBill');
+let data = await getAllProductBill();
+let row = "";
+
+data.forEach((val, id) => {
+    val.products.forEach(product =>{
+        row += `
+        <td>${product.descriptions}</td>
+        <td>${product.quantity}</td>
+        <td>${product.price}</td>
+        <td>${product.total}</td>
+        <tr>
+        </tr>
+        `
+    })
+})
+myBill.innerHTML = row
