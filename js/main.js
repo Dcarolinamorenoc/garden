bd2b8293b6db0e251797d95df146a8d09df284b0
Dcarolinamorenoc <156273221+Dcarolinamorenoc@users.noreply.github.com>
2024-04-23 11:58:54 -0500

    feat: :construction: 3. Devuelve un listado con el nombre, apellidos y email de los empleados cuyo jefe tiene un código de jefe igual a 7.

--- js/main.js
+++ js/main.js
@@ -3,4 +3,10 @@ import {
     getAllOfficesFromSpainCityAndMovil
 } from "./module/offices.js";
 
-console.log(await getAllOfficesFromSpainCityAndMovil());
+// console.log(await getAllOfficesFromSpainCityAndMovil());
+
+import{
+    getAllEmployeesWithBossAndCodeSeven
+} from "./module/employees.js";
+
+console.log(await getAllEmployeesWithBossAndCodeSeven());
---
+++ js/module/employees.js
@@ -0,0 +1,17 @@
+// 3. Devuelve un listado con el nombre, apellidos y email de los empleados cuyo jefe tiene un código de jefe igual a 7.
+
+
+export const getAllEmployeesWithBossAndCodeSeven = async() =>{
+    let rest = await fetch ("http://localhost:5502/employees?code_boss=7")
+    let data = await rest.json();
+    let dataUpdate = [];
+    data.forEach(val => {
+        let [email] = val.email.match(/(?<=\[)[^\[\]]+@[^@\[\]]+(?=\])/)
+        dataUpdate.push({
+            nombre: val.name,
+            apellidos: `${val.lastname1} ${val.lastname2}`,
+            email
+        });
+    });
+    return dataUpdate;
+}
